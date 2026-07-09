ebe5d80e5d763bda1778b0675e2aacc524fd0380
paranietharan <142080100+paranietharan@users.noreply.github.com>
2026-07-09 23:15:16 +0530

    init

---
+++ src/main/java/com/paranietharan/byteblog/ByteblogApplication.java
@@ -0,0 +1,13 @@
+package com.paranietharan.byteblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteblogApplication.class, args);
+    }
+
+}
